0c5f4246474a28f406d703da3109c31c60f78834
m1m0ch1 <lukacsmarci1108@gmail.com>
2024-04-18 09:22:02 +0200

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asd
+{
+    class Eredmeny
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+        public int Percent { get; set; }
+        public int Mark
+        {
+            get
+            {
+                if (Percent < 25)
+                {
+                    Mark = 1;
+                }
+                else if (Percent >= 25 && Percent <= 39)
+                {
+                    Mark = 2;
+                }
+                else if (Percent >= 40 && Percent <= 59)
+                {
+                    Mark = 3;
+                }
+                else if (Percent >= 60 && Percent <= 79)
+                {
+                    Mark = 4;
+                }
+                else if (Percent >= 80 && Percent <= 100)
+                {
+                    Mark = 5;
+                }
+                return Mark;
+            }
+            set { }
+        }
+
+        public override string ToString()
+        {
+            return $"Eredmény: <{FirstName} {LastName}: {Subject}, {Percent}, {Mark}>";
+        }
+
+        public static Eredmeny CreateFromLine(string line, char seperator = ',')
+        {
+            string[] values = line.Split(seperator);
+            return new Eredmeny
+            {
+                Id = int.Parse(values[0]),
+                FirstName = values[1],
+                LastName = values[2],
+                Subject = values[3],
+                Percent = int.Parse(values[4]),
+                Mark = int.Parse(values[5])
+
+            };
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace asd
+{
+    class EredmenyRepository
+    {
+        public static string Path { get; set; }
+        public static bool SkipHeader { get; set; } = true;
+        public static char Separator { get; set; } = ',';
+        public static List<Eredmeny> FindAll()
+        {
+            using (StreamReader reader = new StreamReader(Path))
+            {
+                if (SkipHeader)
+                {
+                    reader.ReadLine();
+                }
+                List<Eredmeny> eredmenyek = new List<Eredmeny>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    Eredmeny eredmeny = Eredmeny.CreateFromLine(line, Separator);
+                    eredmenyek.Add(eredmeny);
+                }
+                return eredmenyek;
+            }
+        }
+    }
+}
